84573060084df738cbe9d670d90bc39f4065e8dc
wangming <wangming.co@hotmail.com>
2025-02-02 10:46:28 +0800

    Update stock.js

--- web/functions/stock.js
+++ web/functions/stock.js
@@ -1,4 +1,10 @@
 export function onRequest(context) {
-  // return fetch('https://api.vc89.online/stock')
-  return new Response([[1,1,1,1,1,1]])
+  try {
+    return fetch('https://api.vc89.online/stock')
+  } catch(error) {
+    console.error('fetch error', error)
+    const errorInfo = `Error Message: ${error.message}\nStack Trace: ${error.stack}`;
+    return Response.json({ message: errorInfo });
+  
+  }
 }
