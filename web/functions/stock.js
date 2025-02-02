dccc85a417fbf26734e2677d32b24d74052534cd
wangming <wangming.co@hotmail.com>
2025-02-02 15:13:15 +0800

    api

--- web/functions/stock.js
+++ web/functions/stock.js
@@ -9,10 +9,18 @@
 //   }
 // }
 
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request));
-});
+// addEventListener('fetch', event => {
+//   event.respondWith(handleRequest(event.request));
+// });
 
-async function handleRequest(request) {
-  return new Response('Send a POST request to trigger a notification.');
+// async function handleRequest(request) {
+//   return new Response('Send a POST request to trigger a notification.');
+// }
+
+export function onRequest(context) {
+  try {
+    return fetch('https://api.vc89.online/stock')
+  } catch(error) {
+    return `Error Message: ${error.message}\nStack Trace: ${error.stack}`;
+  }
 }
