145c9a525cf4c0022c3495c048b0e6536193a966
wangming <wangming.co@hotmail.com>
2025-02-02 21:11:37 +0800

    Update db.js

--- web/functions/db.js
+++ web/functions/db.js
@@ -1,4 +1,4 @@
-export function onRequest(context) {
-  const results = context.env.DB.prepare("SELECT * FROM Bitcoin order by id desc limit 10").all()
+export async function onRequest(context) {
+  const results = await context.env.DB.prepare("SELECT * FROM Bitcoin order by id desc limit 10").all()
 			return Response.json(results);
 }
