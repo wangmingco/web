23aa948e402215fd4cbef20db10eb8e38037fab9
wangming <wangming.co@hotmail.com>
2025-02-03 16:17:07 +0800

    Update App.vue

--- web/src/App.vue
+++ web/src/App.vue
@@ -42,65 +42,11 @@ export default {
   },
   methods: {
     initData() {
-      // this.bondData = fetch('https://api.vc89.online/bond')
-      this.stockData = fetch('/stock')
-      console.log('fetch ', this.stock)
-      // this.goldkData = fetch('https://api.vc89.online/gold')
-      // this.forexData = fetch('https://api.vc89.online/forex')
-      // this.bitcoinData = fetch('https://api.vc89.online/bitcoin')
-    },
-    // generateOHLC(count) {
-    //   let data = [];
-    //   let xValue = +new Date(2011, 0, 1);
-    //   let minute = 60 * 1000;
-    //   let baseValue = Math.random() * 12000;
-    //   let boxVals = new Array(4);
-    //   let dayRange = 12;
-    //   for (let i = 0; i < count; i++) {
-    //     baseValue = baseValue + Math.random() * 20 - 10;
-    //     for (let j = 0; j < 4; j++) {
-    //       boxVals[j] = (Math.random() - 0.5) * dayRange + baseValue;
-    //     }
-    //     boxVals.sort();
-    //     let openIdx = Math.round(Math.random() * 3);
-    //     let closeIdx = Math.round(Math.random() * 2);
-    //     if (closeIdx === openIdx) {
-    //       closeIdx++;
-    //     }
-    //     let volumn = boxVals[3] * (1000 + Math.random() * 500);
-    //     // ['open', 'close', 'lowest', 'highest', 'volumn']
-    //     // [1, 4, 3, 2]
-    //     data[i] = [
-    //       echarts.format.formatTime('yyyy-MM-dd\nhh:mm:ss', (xValue += minute)),
-    //       +boxVals[openIdx].toFixed(2),
-    //       +boxVals[3].toFixed(2),
-    //       +boxVals[0].toFixed(2),
-    //       +boxVals[closeIdx].toFixed(2),
-    //       +volumn.toFixed(0),
-    //       this.getSign(data, i, +boxVals[openIdx], +boxVals[closeIdx], 4) // sign
-    //     ];
-    //   }
-    //   console.log(data)
-    //   return data;
-    // },
-    // getSign(data, dataIndex, openVal, closeVal, closeDimIdx) {
-    //   var sign;
-    //   if (openVal > closeVal) {
-    //     sign = -1;
-    //   } else if (openVal < closeVal) {
-    //     sign = 1;
-    //   } else {
-    //     sign =
-    //       dataIndex > 0
-    //         ? // If close === open, compare with close of last record
-    //         data[dataIndex - 1][closeDimIdx] <= closeVal
-    //           ? 1
-    //           : -1
-    //         : // No record of previous, set to be positive
-    //         1;
-    //   }
-    //   return sign;
-    // }
+      fetch('/stock').then(response => {
+        console.log('fetch ', response)
+        this.stockData = response.json();
+      })
+    }
   }
 }
 </script>
