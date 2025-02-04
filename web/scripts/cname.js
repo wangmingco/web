63b89bc3c6353151aa0a6ee27d594b22805e5d7b
wangming <wangming.co@hotmail.com>
2025-02-04 10:13:57 +0800

    cname

--- web/scripts/cname.js
+++ web/scripts/cname.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 console.log("Arguments:", process.argv.slice(2));
 
-const cnameContent = 'www.vc89.online';
+const cnameContent = 'vc89.online';
 
 const cnamePath = path.resolve(__dirname, '../dist/CNAME');
 fs.writeFileSync(cnamePath, cnameContent);
--- web/scripts/cname_cn.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-console.log("Arguments:", process.argv.slice(2));
-
-const cnameContent = 'www.vc89.cn';
-
-const cnamePath = path.resolve(__dirname, '../dist/CNAME');
-fs.writeFileSync(cnamePath, cnameContent);
