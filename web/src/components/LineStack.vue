1e8e4a9fda64bd1f9b354a559238ae6c3a2ee1f5
wangming <wangming.co@hotmail.com>
2025-02-06 16:57:33 +0800

    Update LineStack.vue

--- web/src/components/LineStack.vue
+++ web/src/components/LineStack.vue
@@ -35,7 +35,7 @@ export default {
   },
   methods: {
     initChart(val) {
-      
+
       const title = this.title + " " + val.dateArray[val.dateArray.length - 1]
       var option = {
         title: {
@@ -45,7 +45,7 @@ export default {
           trigger: 'axis'
         },
         legend: {
-          data: [ 'm3', 'm6', 'y1','y2', 'y3', 'y5', 'y7', 'y10', 'y30']
+          data: ['m3', 'm6', 'y1', 'y2', 'y3', 'y5', 'y7', 'y10', 'y30']
         },
         grid: {
           left: '3%',
@@ -64,15 +64,51 @@ export default {
           data: val.dateArray
         },
         yAxis: {
-          type: 'value'
+          type: 'value',
+          // 自定义 y 轴的最小值和最大值
+          min: 0, // 设置最小值
+          max: 5, // 设置最大值
+          splitLine: {
+            lineStyle: {
+              type: 'solid' // solid: 实线, dotted: 点线, dashed: 虚线
+            }
+          }
         },
         series: val.dataArray
       };
+
+      // 计算最大值和最小值
+      // 提取所有银行的数据
+      const allData = data.dataArray.m3
+        .concat(data.dataArray.m5)
+        .concat(data.dataArray.y1)
+        .concat(data.dataArray.y2)
+        .concat(data.dataArray.y3)
+        .concat(data.dataArray.y5)
+        ;
+
+      // 提取所有数据点
+      const allDataPoints = allData.map(item => item.data[0]);
+
+      // 找到最大值和最小值
+      const maxY = Math.max(...allDataPoints);
+      const minY = Math.min(...allDataPoints);
+
+      // 设置 y 轴范围
+      option.yAxis.min = this.decreaseAndFloor(minY);
+      option.yAxis.max = maxY;
 
       if (option && typeof option === 'object') {
         this.myChart.setOption(option);
       }
 
+    },
+    decreaseAndFloor(value) {
+      // 数值减1后向下取整
+      let result = Math.floor(value - 1);
+
+      // 如果结果小于0，则返回0
+      return result < 0 ? 0 : result;
     }
 
   }
